7b2f2a4e2333e3b43fda1f174b4f9dba1d23c8ea
Henrik Berg <khp.berg@gmail.com>
2023-01-24 16:59:59 +0100

    Solved first part
    ParseMessage working

--- week2/src/LogAnalysis.hs
+++ week2/src/LogAnalysis.hs
@@ -1,6 +1,14 @@
+{-# OPTIONS_GHC -Wall #-}
+
 module LogAnalysis (someFunc) where
 
 import           Log
 
+parseMessage :: [String] -> LogMessage
+parseMessage ("E" : c : t : m) = LogMessage (Error (read c)) (read t) (unwords m)
+parseMessage ("W" : t : m) = LogMessage Warning (read t) (unwords m)
+parseMessage ("I" : t : m) = LogMessage Info (read t) (unwords m)
+parseMessage p = Unknown (unwords p)
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = print $ parseMessage $ words "E 2 562 help help"
